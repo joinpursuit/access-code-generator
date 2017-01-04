611728a7ad1be4d5b575826b5ecf06352003b263
jondubya86 <jonathanwu86@gmail.com>
2017-01-04 11:38:55 -0500

    sequelize cli db with intro models

---
+++ back/db/migrations/20170104163744-create-message.js
@@ -0,0 +1,27 @@
+'use strict';
+module.exports = {
+  up: function(queryInterface, Sequelize) {
+    return queryInterface.createTable('Messages', {
+      id: {
+        allowNull: false,
+        autoIncrement: true,
+        primaryKey: true,
+        type: Sequelize.INTEGER
+      },
+      msg: {
+        type: Sequelize.STRING
+      },
+      createdAt: {
+        allowNull: false,
+        type: Sequelize.DATE
+      },
+      updatedAt: {
+        allowNull: false,
+        type: Sequelize.DATE
+      }
+    });
+  },
+  down: function(queryInterface, Sequelize) {
+    return queryInterface.dropTable('Messages');
+  }
+};
