8fabdd3cdebd7a644bc005976e3ae07ab3303db8
jondubya86 <jonathanwu86@gmail.com>
2017-01-05 10:41:17 -0500

    minor adjustments to chatroom routes

--- back/server/routes/chatroom-router.js
+++ back/server/routes/chatroom-router.js
@@ -1,7 +1,7 @@
 const router = require("express").Router();
 const Chatroom = require('../../db').Chatroom;
 
-
+//new chat room
 const createChat = (req,res) => {
 	Chatroom.create({
 		name: req.body.chatroom
@@ -10,12 +10,15 @@ const createChat = (req,res) => {
 		res.send('Chatroom ' + req.body.chatroom + ' created!'))
 };
 
+//displays all chats on sidebar of active chatrooms to user
 const getAllChats = (req,res) => (
 	Chatroom.findAll()
 	.then((chatrooms)=>
 		res.send(chatrooms))
 );
 
+
+//used for when user enters a room already created to join convo
 const getSingleRoom = (req,res) => (
 	Chatroom.findOne({
 		where: {id: req.params.chatroomId}
@@ -25,11 +28,24 @@ const getSingleRoom = (req,res) => (
 );
 
 
-//figure out formula to what happens when user leaves room, if user is last person in room then the room closes. 
+//figure out function to what happens when user leaves room, if user is last person in room then the room closes. 
+
+include: [{
+        model: Tool,
+        as: 'Instruments',
+        where: { name: { $like: '%ooth%' } }
+    }]
+}).then(function(users) {
+    console.log(JSON.stringify(users))
 
 const exitRoom = (req,res)=> (
 	Chatroom.destroy({
 		where: {id: req.params.chatroomId}
+// 		include: [{
+// 			model: User,
+// //			as: tbd,
+// 			where: {}
+// 		}]
 	})
 	.then((chat)=>
 		res.send('Chatroom '+chatId+' deleted!'))
